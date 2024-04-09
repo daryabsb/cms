3a2ded2552b53dbf22e31655b80a859b27fc2f84
Darya Ibrahim <daryabsb@gmail.com>
2024-04-09 11:56:26 +0300

    Fixed home-three industico

--- src/industico/urls.py
+++ src/industico/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path ,include
+from django.urls import path, include
 from src.industico import views
 
 
-
-app_name='frontend'
+app_name = 'frontend'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home-three/', views.index3, name='index'),
 ]
--- src/industico/views.py
+++ src/industico/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 
-
 def index(request):
     # config_data = setup_config.loadConfig()
-    theme_value = 'industico' # config_data.get('Theme', {}).get('value', 'theme5')
+    # config_data.get('Theme', {}).get('value', 'theme5')
+    theme_value = 'industico'
     template_name = f"{theme_value}/home/index.html"
     # config = setup_config.loadConfig()
     # query = request.GET.get('search')
@@ -17,9 +17,31 @@ def index(request):
     #         render_data = page_detail(request)
     # if config['Reading']['show_on_front']['value'] == 'Blog':
     #     render_data = blog_list(request)
-    context={
-        "blogs":[],  
-        "banner_title":"Blogs",
-        "query":'query'    
-    }  
+    context = {
+        "blogs": [],
+        "banner_title": "Blogs",
+        "query": 'query'
+    }
+    return render(request, template_name, context)
+
+
+def index3(request):
+    # config_data = setup_config.loadConfig()
+    # config_data.get('Theme', {}).get('value', 'theme5')
+    theme_value = 'industico'
+    template_name = f"{theme_value}/home/home-three.html"
+    # config = setup_config.loadConfig()
+    # query = request.GET.get('search')
+    # if config['Reading']['show_on_front']['value'] == 'Page':
+    #     if query:
+    #         render_data = blog_list(request)
+    #     else:
+    #         render_data = page_detail(request)
+    # if config['Reading']['show_on_front']['value'] == 'Blog':
+    #     render_data = blog_list(request)
+    context = {
+        "blogs": [],
+        "banner_title": "Blogs",
+        "query": 'query'
+    }
     return render(request, template_name, context)
