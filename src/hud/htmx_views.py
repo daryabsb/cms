11d876fc20cf4999ebe3c3f15da040c322d435f2
Darya Ibrahim <daryabsb@gmail.com>
2024-05-14 14:01:43 +0300

    added some OOB to the update quantity

--- src/hud/htmx_views.py
+++ src/hud/htmx_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from src.hud.models import ProductGroup, Product, Barcode, PosOrderItem
+from src.hud.models import ProductGroup, Product, Barcode, PosOrderItem, PosOrder
 
 
 def modal_product(request, id):
@@ -15,13 +15,23 @@ def add_quantity(request, item_number):
     item = get_object_or_404(PosOrderItem, number=item_number)
     item.quantity += 1  # Set quantity to the new value received from the client
     item.save()
-    return render(request, 'hud/pos/partials/order-item-detail.html', {"item":item})
+    active_order = PosOrder.objects.filter(is_active=True).first()
+    context = {
+        "item": item,
+        "active_order": active_order,
+    }
+    return render(request, 'hud/pos/renders/update-order-item.html', context)
 
 def subtract_quantity(request, item_number):
     item = get_object_or_404(PosOrderItem, number=item_number)
+    active_order = PosOrder.objects.filter(is_active=True).first()
+    context = {
+        "item": item,
+        "active_order": active_order,
+    }
     if item.quantity > 1:
         item.quantity -= 1
         item.save()
-        return render(request, 'hud/pos/partials/order-item-detail.html', {"item":item})
+        return render(request, 'hud/pos/renders/update-order-item.html', context)
     elif item.quantity == 1:
-        return render(request, 'hud/pos/renders/confirm-remove.html', {"item":item})
+        return render(request, 'hud/pos/renders/confirm-remove.html', context)
